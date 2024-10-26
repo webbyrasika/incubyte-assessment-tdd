ad2f6bb68027a3c33ee3745a2911ef0acb5efb7f
webbyrasika <rvtembhare2605@gmail.com>
2024-10-26 13:06:34 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function strCalculator(inputString) {
+  try {
+    let result;
+    let str = inputString.replace("//;\n,-", " ");
+    console.log(str, "str");
+    result = eval(str);
+
+    console.log(result);
+  } catch {
+    console.error("negative numbers are not allowed");
+  }
+  // console.log(inputString);
+}
+
+strCalculator("1,5");
